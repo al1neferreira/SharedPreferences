2f200e342b28cc95d6d3f0cfdf9fd5578fcedda8
Aline Ferreira <alinfst@gmail.com>
2024-08-18 13:04:10 -0300

    Initial commit

---
+++ app/src/main/java/com/example/sharedpreferences/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.sharedpreferences
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.viewbinding.ViewBindings
+import com.example.sharedpreferences.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var sharedPreferences: SharedPreferences
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        sharedPreferences = getSharedPreferences("NoteData", Context.MODE_PRIVATE)
+    }
+}
